07576e5f364028c2b327f380916abf14b5531f13
spansoft <59381097+spansoft@users.noreply.github.com>
2024-09-13 07:47:45 -0400

    commented brochure link

--- src/Pages/Navigate.js
+++ src/Pages/Navigate.js
@@ -3,11 +3,11 @@ import { Outlet, Link } from "react-router-dom";
 
 function Navigate() {
   return (
-    <div>
+    <div align="center">
       <center>
       <table>
         <tr>
-          <td class="h4 JJ-font"><Link to="/" class="JJ-font-white">Home</Link> | <Link to="/Brochure1" class="JJ-font-white">Brochure</Link> | <Link to="/Entrance" class="JJ-font-white">How to Enter</Link> | <Link to="/Parking" class="JJ-font-white">Parking Pass</Link></td>
+          <td class="h4 JJ-font"><Link to="/" class="JJ-font-white">Home</Link> | <Link to="/Entrance" class="JJ-font-white">How to Enter</Link> | <Link to="/Parking" class="JJ-font-white">Parking Pass</Link></td>
         </tr>
       </table>
       </center>
