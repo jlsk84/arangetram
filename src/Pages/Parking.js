a1a5f6b87bc35cfab0284b3316c316dbdd24a114
spansoft <59381097+spansoft@users.noreply.github.com>
2024-09-14 17:37:21 +0200

    updated parking info

--- src/Pages/Parking.js
+++ src/Pages/Parking.js
@@ -4,7 +4,7 @@ function Parking() {
     return (
         <div className="Parking">
             <p class="h4 JJ-font">Parking Pass</p>
-            <p class="h5 JJ-font">Please <Link to="/P3_Parking_Placard.pdf" class="JJ-font-white">download</Link> below image and print the parking pass</p>
+            <p class="h5 JJ-font">Please <Link to="/P3_Parking_Placard.pdf" class="JJ-font-white">download</Link> below image and print the parking pass or show the parking pass below at the P3 parking entrance</p>
             <img src="Placard.png" class="Img-sizer border border-dark rounded" alt="P3 parking pass"></img>
         </div>
     );
